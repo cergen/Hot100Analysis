b472034cf91ee9175fd512b0717be8226d2f219f
Can Ergen <can_ergen11@hotmail.com>
2022-04-11 10:55:40 -0400

    update

--- Hot100.py
+++ Hot100.py
@@ -9,19 +9,59 @@ pd.set_option('display.max_rows', 100)
 #dataset obtained from https://www.kaggle.com/datasets/dhruvildave/billboard-the-hot-100-songs
 dfHot100 = pd.read_csv('D:\Python Coding\Hot100Analysis\hot100.csv')
 
-#dataset for Artist Genre match obtained from
-dfArtistGenre = pd.read_csv('D:\Python Coding\Hot100Analysis\MTV10000Artists\ArtistGenre_csv.csv')
-dfArtistGenre
 
+#top 10 df
 columnsToDrop = ['last-week','peak-rank','weeks-on-board']
 dfHot100.drop(columnsToDrop, axis = 1, inplace = True)
 dfHot100 = dfHot100[dfHot100['rank'] < 11]
 dfHot100
 
-print(list(dfArtistGenre.columns))
-columnsToDrop2 = ['facebook','twitter','website','mtv']
-dfArtistGenre.drop(columnsToDrop2, axis = 1, inplace = True)
-dfArtistGenre
+#top 10 unique artist and songs
+unqartist = dfHot100['artist'].unique()
+len(unqartist)
+artistCount = dfHot100['artist'].count()
+artistCount
 
-dfJoined = pd.concat([dfHot100, dfArtistGenre], axis=1, join="inner")
-dfJoined
+#top 1 df
+top1 = dfHot100[dfHot100['rank'] < 2]
+top1
+
+# 749 unique artists
+top1['artist'].unique()
+len(top1['artist'].unique())
+
+# 1104 unique songs
+top1['song'].unique()
+len(top1['song'].unique())
+
+titleWordCount = []
+for track in top1['song']:
+    trackL = track.lower().split()
+    titleWordCount.extend(trackL)
+    
+dfWords = pd.DataFrame(titleWordCount, columns=['Words'])
+dfWords.info()
+countColumn = []
+for i in range(len(dfWords['Words'])):
+    countColumn.append(1)
+dfWords['Count'] = countColumn
+dfWords['Words'] = titleWordCount
+dfWords.info()
+dfWords.describe()
+dfWords['Words'].unique()
+wordCount = dfWords.groupby('Words').sum().sort_values(by=['Count'], ascending=False)
+wordCount
+
+
+#Visualize Word Count
+fig, ax = plt.subplots(figsize=(8, 8))
+
+# Plot horizontal bar graph
+dfWords.sort_values(by='Count').plot.barh(x='Words',
+                      y='Count',
+                      ax=ax,
+                      color="purple")
+
+ax.set_title("Top Words in Billboard 100 Chart Toppers")
+
+plt.show()
